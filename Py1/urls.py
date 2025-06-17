d509e0b1e918cccc4158bf8aa3a5544757e9788a
marck-h-cmd <mahermenegildopa@unitru.edu.pe>
2025-06-17 10:31:40 -0500

    fix: Python urls

--- Py1/urls.py
+++ Py1/urls.py
@@ -16,10 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
-from seguridadApp import urls
-from ventasApp import urls
+
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('',include(urls)),
+    path('admin/', admin.site.urls), 
+    path('', include('seguridadApp.urls')),  
+    path('ventas/', include('ventasApp.urls')),
     
 ]
