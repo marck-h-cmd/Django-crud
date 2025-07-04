845eef4449ef2bdadda4e99a8133793057061d5b
marck-h-cmd <mahermenegildopa@unitru.edu.pe>
2025-07-04 18:53:13 -0500

    chore:whitenoise

--- Py1/settings.py
+++ Py1/settings.py
@@ -143,11 +143,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'Py1/static'),
     os.path.join(BASE_DIR, 'gestionVentas/static'),
     os.path.join(BASE_DIR, 'seguridadApp/static'),  
-    os.path.join(BASE_DIR, 'ventasApp/static'),     
+
 ]
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
